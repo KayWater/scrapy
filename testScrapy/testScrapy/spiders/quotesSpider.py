8fd8888cccd9aa498d638c29130db5e050c7d072
iw12082 <chenjie@icewing.com>
2018-09-06 08:47:16 +0800

    add author parse

--- testScrapy/testScrapy/spiders/quotesSpider.py
+++ testScrapy/testScrapy/spiders/quotesSpider.py
@@ -4,6 +4,8 @@ Created on 2018年9月5日
 @author: iw12082
 '''
 import scrapy
+from _ast import Call
+from attr import __author__
 
 
 class QuotesSpider(scrapy.Spider):
@@ -11,15 +13,39 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://quotes.toscrape.com',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        # follow links to author page
+        for author_link in response.css("small.author + a::attr(href)"):
+            yield response.follow(author_link, self.author_parse)
+        
+        # follow pagination links
+        for link in response.css("li.next a::attr(href)"):
+            yield response.follow(link, self.parse)
+        
+#         for quote in response.css("div.quote"):
+#             yield {
+#                 "text": quote.css("span.text::text").extract_first(),
+#                 "author": quote.css("small.author::text").extract_first(),
+#                 "tags": quote.css("div.tags a.tag::text").extract()
+#             }
+#             
+#         next_page = response.css("li.next a::attr(href)").extract_first() 
+#         if next_page is not None:
+#             next_page = response.urljoin(next_page)
+#             yield scrapy.Request(next_page, callback=self.parse)
+            
+    def author_parse(self, response):
+        def extract_with_css(query):
+            return response.css(query).extract_first().strip()
+        
+        yield {
+            "name": extract_with_css("h3.author_title"),
+            "birthdate": extract_with_css("span.author-born-date"),
+            "bio": extract_with_css("div.author-description"),
+            }
+        
